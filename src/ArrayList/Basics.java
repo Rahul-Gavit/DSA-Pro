10a8e3ba494602b270ca6c248792e08cf4bc91dd
Rahul Gavit <rahul@easyhrworld.com>
2024-08-09 15:07:45 +0530

    refactor: Improve ArrayList methods and add new functionalities
    - Refactor the `printNumbers` method to include time and space complexity comments
    - Refactor the `largestString` method to include time and space complexity comments
    - Refactor the `reverseList` method to include time and space complexity comments
    - Add a new method `removeDuplicates` to remove all occurrences of a given element from an ArrayList
    - Add a new method `maxAndMin` to find the maximum and minimum values in an ArrayList of integers
    - Add a new method `sortStringList` to sort an ArrayList of strings
    
    These changes improve the code structure and add new functionalities to the ArrayList class.

--- src/ArrayList/Basics.java
+++ src/ArrayList/Basics.java
@@ -7,7 +7,7 @@ public class Basics {
     // Write a Java program to create an ArrayList of integers and add 10 elements
     // to it. Then, print all the elements.
 
-    public static void printNumbers(int num) {
+    public static void printNumbers(int num) { // Time Complexity: O(n) Space Complexity: O(1)
 
         ArrayList<Integer> list = new ArrayList<Integer>();
         Scanner sc = new Scanner(System.in);
@@ -22,7 +22,7 @@ public class Basics {
 
     // Write a method that takes an ArrayList of strings and returns the length of
     // the longest string.
-    public static int largestString(int num) {
+    public static int largestString(int num) { // Time Complexity: O(n) Space Complexity: O(1)
         ArrayList<String> list = new ArrayList<String>();
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < num; i++) {
@@ -40,7 +40,7 @@ public class Basics {
 
     // Implement a method that reverses the elements of an ArrayList.
 
-    public static void reverseList(ArrayList<String> list) {
+    public static void reverseList(ArrayList<String> list) { // Time Complexity: O(n) Space Complexity: O(1)
         int left = 0;
         int right = list.size() - 1;
 
@@ -56,18 +56,81 @@ public class Basics {
         }
     }
 
+    // Write a method to remove all occurrences of a given element from an
+    // ArrayList.
+
+    public static void removeDuplicates(ArrayList<Integer> num) { // Time Complexity: O(n^2) Space Complexity: O(1)
+        for (int i = 0; i < num.size(); i++) {
+            for (int j = i + 1; j < num.size(); j++) {
+                if (num.get(i) == num.get(j)) {
+                    num.remove(j);
+                }
+            }
+        }
+    }
+
+    // Write a program to find the maximum and minimum values in an ArrayList of
+    // integers.
+
+    public static void maxAndMin(ArrayList<Integer> num) { // Time Complexity: O(n) Space Complexity: O(1)
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int i = 0; i < num.size(); i++) {
+            if (num.get(i) > max) {
+                max = num.get(i);
+            } else if (num.get(i) < min) {
+                min = num.get(i);
+            }
+        }
+        System.out.println("Max: " + max + " Min: " + min);
+    }
+
+    public static void sortStringList(ArrayList<String> list) { // Time Complexity: O(n^2) Space Complexity: O(1)
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i).compareTo(list.get(j)) > 0) {
+                    String temp = list.get(i);
+                    list.set(i, list.get(j));
+                    list.set(j, temp);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // printNumbers(10);
-        // System.out.println(largestString(5));
 
         ArrayList<String> list = new ArrayList<>();
         list.add("Apple");
-        list.add("Banana");
         list.add("Orange");
+        list.add("Banana");
         list.add("Mango");
 
-        System.out.println("Original List: " + list);
-        reverseList(list);
-        System.out.println("Reversed List: " + list);
+        ArrayList<Integer> num = new ArrayList<>();
+        num.add(1);
+        num.add(2);
+        num.add(3);
+        num.add(2);
+        num.add(4);
+        num.add(5);
+        num.add(1);
+
+        // System.out.println(largestString(5));
+
+        // System.out.println("Original List: " + list);
+        // reverseList(list);
+        // System.out.println("Reversed List: " + list);
+
+        // System.out.println("Original List: " + num);
+        // removeDuplicates(num);
+        // System.out.println("List after removing duplicates: " + num);
+
+        // maxAndMin(num);
+
+        // System.out.println("Original List: " + list);
+        // sortStringList(list);
+        // System.out.println("Sorted List: " + list);
+
     }
 }
